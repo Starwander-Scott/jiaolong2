7aca42c0d71273593c2e5a259b99c45a9578a1a0
cyc <cycsjtuer@sjtu.edu.cn>
2025-10-26 20:03:29 +0800

    使用DMA方法，实现了对于遥控器数据接受，同时也提供了rc->遥控器开关，摇杆数据的初步解算

---
+++ MDK-ARM/C3/rc.d
@@ -0,0 +1,10 @@
+c3/rc.o: ..\Core\Src\rc.cpp ..\Core\Inc\rc.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__config \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\cstring \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__assert \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__verbose_abort \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\__availability \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\libcxx\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h
